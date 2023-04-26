24dc2b833810c9359977e58227eac0b2b0c167bc
w3eydi <w3.eydi@gmail.com>
2023-04-26 22:41:44 +0300

    Animator added.

--- KitchenChaos/Assets/Scripts/Player.cs
+++ KitchenChaos/Assets/Scripts/Player.cs
@@ -6,6 +6,8 @@ public class Player : MonoBehaviour
 {
     [SerializeField] private float moveSpeed = 7f;
 
+    private bool isWalking;
+
     private void Update() {
         Vector2 inputVector = new Vector2(0, 0);
 
@@ -26,6 +28,14 @@ public class Player : MonoBehaviour
         Vector3 moveDirection = new Vector3(inputVector.x, 0f, inputVector.y);
         transform.position += moveDirection * moveSpeed * Time.deltaTime;
 
-        Debug.Log(Time.deltaTime);
+        isWalking = moveDirection != Vector3.zero;
+
+        float rotateSpeed = 11f;
+        transform.forward = Vector3.Slerp(transform.forward, moveDirection, Time.deltaTime * rotateSpeed);
+
+    }
+
+    public bool IsWalking() {
+        return isWalking;
     }
 }
